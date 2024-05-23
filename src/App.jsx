ced51a175ff6e5fdf43d76505f68ab3d2f464df0
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-24 00:43:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss"
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -7,6 +8,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 const App = () => {
   return <div>
+        <Cursor />
     <section id="Homepage">
       <Navbar />
       <Hero />
