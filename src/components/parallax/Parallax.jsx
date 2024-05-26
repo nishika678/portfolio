b6001164cd13eb6c5f6c0c655e0bfe6bdca83adf
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-05-26 20:34:57 +0530

    Update Parallax.jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -3,27 +3,27 @@ import "./parallax.scss"
 import {motion,useScroll,useTransform} from "framer-motion"
 const Parallax=({type})=> {
 
-    const ref=useRef()
+    const par=useRef()
 
     const {scrollYProgress}=useScroll({
-        target:ref,
+        target:par,
         offset:["start start","end start"]
     });
 
     const yText=useTransform(scrollYProgress,[0,1],["0%","500%"])
 
-    const yBg=useTransform(scrollYProgress,[0,1],["0%","100%"])
+    const yB=useTransform(scrollYProgress,[0,1],["0%","100%"])
 
   return (
-    <div className="parallax" ref={ref} style={{background:
+    <div className="parallax" ref={par} style={{background:
         type==="services" ? "linear-gradient(180deg,#111132,#0c0c1d)" :
      "linear-gradient(180deg,#111132,#505064)",   
     }}>
-        <motion.h1 style={{y:yText}}>{type=="services" ? "What We Do?" : "What We Did?"}</motion.h1>
+        <motion.h1 style={{y:yText}}>{type=="services" ? "What I Do?" : "What I Did?"}</motion.h1>
         <motion.div className="mountains">
         </motion.div>
-        <motion.div className="planets" style={{ y:yBg, backgroundImage:`url(${type==="services"?"/planets.png":"/sun.png"})`}}></motion.div>
-        <motion.div style={{x:yBg}}className="stars"></motion.div>
+        <motion.div className="planets" style={{ y:yB, backgroundImage:`url(${type==="services"?"/planets.png":"/sun.png"})`}}></motion.div>
+        <motion.div style={{x:yB}}className="stars"></motion.div>
     </div>
   )
 }
